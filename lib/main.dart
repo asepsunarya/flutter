cf37da44db29305afc9fce437450be5ee9c6cf06
asepsunarya <asepsunarya839@gmail.com>
2024-03-06 14:28:46 +0700

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text("Hey ninjas!")
+    home: Home()
   ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'InterRoman'
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.red[600],
+        child: const Text('click me'),
+      ),
+    );
+  }
 }
